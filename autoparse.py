7410e3d9c8c0edad0309f696f935cd417f46ae16
Aschouwe <87235430+aschouwe@users.noreply.github.com>
2021-12-15 23:23:43 -0700

    Update autoparse.py
    Proof of Concept

--- autoparse.py
+++ autoparse.py
@@ -6,6 +6,14 @@ import os
 #variable for any pcap file 
 file_name = sys.argv[1]
 
+#create exports directory 
+cmd_dir = 'mkdir exports'
+print("                            ")
+print("****************************")
+print('"exports" Directory Created!')
+print("****************************")
+os.system(cmd_dir)
+
 #parse all src ips sort and count them
 cmd = 'tshark -r ' + str(file_name) + ' -Y tcp -T fields -e ip.src | sort | uniq -c | sort -n'
 #parse all dst ips sort and count them
@@ -16,10 +24,12 @@ cmd3 = 'tshark -r ' + str(file_name) + ' -Y tcp -T fields -e tcp.srcport | sort 
 cmd4 = 'tshark -r ' + str(file_name) + ' -Y tcp -T fields -e tcp.dstport | sort | uniq -c | sort -n'
 # parse all http traffic 
 cmd5 = 'tshark -r ' + str(file_name) + ' -Y http | sort -n'
-#print a space to separate headers
+#export http objects with std out to dev null
+cmd6 = 'tshark -r ' + str(file_name) + ' --export-object "http,/home/kali/finalproject/exports" > /dev/null'
+
 
 print("---------------------")
-print("SOURCE IPs           ")
+print("SOURCE IPs")
 print("---------------------")
 print("Count | Ips          ")
 print("---------------------")
@@ -63,3 +73,11 @@ print("ALL HTTP TRAFFIC     ")
 print("---------------------")
 #call tshark shell command(5)
 os.system(cmd5)
+
+#print a space to separate headers
+print("                              ")
+print("******************************")
+print("HTTP OBJECTS EXPORTED TO FILE!")
+print("******************************")
+#call tshark shell command(5)
+os.system(cmd6)
